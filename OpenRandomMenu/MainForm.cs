a841d8bec239dca1fce9e264aadbb7b8ed8f471e
Victor <publicdomainvic@gmail.com>
2022-06-02 09:24:39 -0400

    Add OnOriginalThreadDonationCodercomToolStripMenuItemClick()

--- OpenRandomMenu/MainForm.cs
+++ OpenRandomMenu/MainForm.cs
@@ -121,7 +121,8 @@ namespace OpenRandomMenu
         /// <param name="e">Event arguments.</param>
         private void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open original thread
+            Process.Start("https://www.donationcoder.com/forum/index.php?topic=17316.0");
         }
 
         /// <summary>
